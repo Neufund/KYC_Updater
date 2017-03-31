4ab74885174d1416e83132272804d3a6101a1bc0
Leonid Logvinov <logvinov.leon@gmail.com>
2017-03-31 13:53:31 +0200

    Add config files

---
+++ config.py
@@ -0,0 +1,6 @@
+SF_INSTANCE = "eu11.salesforce.com"
+SF_USERNAME = "remco+salesforce-api@neufund.org"
+
+REQUIRED_ENV_CONFIG_FIELDS = ["SF_PASSWORD", "SF_TOKEN"]
+
+WEB3_ENDPOINT = "http://localhost:8545"
